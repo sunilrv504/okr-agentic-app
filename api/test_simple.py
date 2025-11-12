5ef9fd928de430e9e06e6f7e00692142f99e1d9e
OKR App Developer <developer@okrapp.com>
2025-11-12 07:09:36 +0530

    Add routing config and simple test function format

---
+++ api/test_simple.py
@@ -0,0 +1,12 @@
+def handler(request):
+    """Simple test function for Vercel"""
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type'
+        },
+        'body': '{"status": "working", "message": "Simple function format test"}'
+    }
